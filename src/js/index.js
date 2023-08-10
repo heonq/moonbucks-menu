0c6ed22900c55d617774172314e8975536400d50
heonq <gkagjsrb@naver.com>
2023-08-10 15:17:26 +0900

    feat: 값을 입력하고 확인 버튼을 누르면 메뉴 추가하는 기능 구현

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,43 @@
-import $ from "../../utils/index";
+import $ from "../../utils/index.js";
+const menuInput = $("#espresso-menu-name");
 
-class App {}
+class App {
+  menuItemTemplate(menuName) {
+    return `<li class="menu-list-item d-flex items-center py-2">
+        <span class="w-100 pl-2 menu-name">${menuName}</span>
+        <button
+          type="button"
+          class="bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button"
+        >
+          수정
+        </button>
+        <button
+          type="button"
+          class="bg-gray-50 text-gray-500 text-sm menu-remove-button"
+        >
+          삭제
+        </button>
+      </li>`;
+  }
+
+  handleInput() {
+    const menuName = menuInput.value;
+    if (menuName === "") return;
+    $("#espresso-menu-list").insertAdjacentHTML(
+      "beforeend",
+      this.menuItemTemplate(menuName)
+    );
+  }
+  init() {
+    $("#espresso-menu-form").addEventListener("submit", (e) =>
+      e.preventDefault()
+    );
+    $("#espresso-menu-submit-button").addEventListener(
+      "click",
+      this.handleInput.bind(this)
+    );
+  }
+}
+
+const app = new App();
+app.init();
